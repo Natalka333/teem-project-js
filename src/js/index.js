8ff1e370247976fec233b5a6571454bfd58a97ab
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-08-18 23:00:04 +0300

    add files

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,51 @@
-(() => {
-  const refs = {
-    openOrgerBtn: document.querySelector('[data-order-open]'),
-    closeOrgerBtn: document.querySelector('[data-order-close]'),
-    order: document.querySelector('[data-order]'),
-  };
+import {} from './modal';
+import 'swiper/swiper-bundle.min.css'; // Импорт стилей Swiper.js
+import Swiper from 'swiper'; // Импорт Swiper.js без бандла
 
-  refs.openOrgerBtn.addEventListener('click', toggleOrger);
-  refs.closeOrgerBtn.addEventListener('click', toggleOrger);
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper-container', {
+    // Опции Swiper
+  });
+});
+// const masterClasses = [
+//   {
+//     title: 'Мастер-класс 1',
+//     imageUrl: 'ссылка_на_изображение_1',
+//     link: 'ссылка_на_мастер_класс_1',
+//   },
+//   {
+//     title: 'Мастер-класс 2',
+//     imageUrl: 'ссылка_на_изображение_2',
+//     link: 'ссылка_на_мастер_класс_2',
+//   },
+//   // ...
+// ];
 
-  function toggleOrger() {
-    refs.order.classList.toggle('is-hidden');
-    document.body.classList.toggle('no-scroll');
-  }
-})();
+// // Создание слайдов с данными о мастер-классах
+// const slideHTML = masterClasses
+//   .map(
+//     masterClass => `
+//     <div class="swiper-slide">
+//       <a href="${masterClass.link}">
+//         <img src="${masterClass.imageUrl}" alt="${masterClass.title}">
+//       </a>
+//     </div>
+//   `
+//   )
+//   .join('');
+
+// // Инициализация Swiper
+// document.addEventListener('DOMContentLoaded', function () {
+//   const swiper = new Swiper('.swiper-container', {
+//     slidesPerView: 1,
+//     spaceBetween: 10,
+//     loop: true,
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//   });
+
+//   // Добавление слайдов в контейнер
+//   swiper.insertAdjacentHTML(slideHTML);
+// });
