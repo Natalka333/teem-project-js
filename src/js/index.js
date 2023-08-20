3673f906e4b81356d3039008931d8bf210ba9013
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-08-20 23:13:27 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -21,18 +21,19 @@ function createMarkupSwiper(arrSliders) {
     arrSliders
       .map(
         ({ cook, topic }) =>
-          `<div class="swiper-slide" id="twist"> 
-            <div class="hero">
-              <div class="cook" style="background-image: url(${cook.imgUrl});"></div>
-              <div class="preview-dish-card">
-                <div class="preview-dish" style="background-image: url(${topic.previewUrl});"></div>
-                <p class="dish-name">${topic.name}</p>
-                <p class="dish-area">${topic.area}</p>
-              </div>
-              <div class="dish" style="background-image: url(${topic.previewUrl});"></div>
-            </div>
-          </div>`
+          `<div class="hero_swiper-slide">
+                <div class=" hero_slider-images">
+                    <div class="hero_image-container-1"><img class="image-1" src=${cook.imgUrl} alt=${cook.name}></div>
+                    <div class="hero_image-container-2">
+                        <img class="hero_image-2" src=${topic.previewUrl} alt=${cook.name}>
+                        <h2 class="hero_image-title">${topic.name}</h2>
+                        <p class="hero_image-descraption">${topic.area}</p>
+                    </div>
+                    <div class="hero_image-container-3"><img class="image-3" src=${topic.imgUrl} alt=${cook.name}></div>
+                </div>
+            </div>`
       )
+
       .join('')
   );
 }
@@ -51,7 +52,7 @@ function createMarkupSwiper(arrSliders) {
 //   loop: true,
 // });
 
-const swiper = new Swiper('image-slider', {
+const swiper = new Swiper('hero', {
   // Optional parameters
   direction: 'vertical',
   loop: true,
@@ -61,13 +62,6 @@ const swiper = new Swiper('image-slider', {
     el: '.swiper-pagination',
     clickable: true,
   },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
@@ -108,3 +102,15 @@ const swiper = new Swiper('image-slider', {
 //       .join('')
 //   );
 // }
+
+// {/* <div class="swiper-slide" id="twist"> <!-- Исправлено здесь -->
+// //             <div class="hero">
+// //               <div class="cook" style="background-image: url(${cook.imgUrl});"></div>
+// //               <div class="preview-dish-card">
+// //                 <div class="preview-dish" style="background-image: url(${topic.previewUrl});"></div>
+// //                 <p class="dish-name">${topic.name}</p>
+// //                 <p class="dish-area">${topic.area}</p>
+// //               </div>
+// //               <div class="dish" style="background-image: url(${topic.previewUrl});"></div>
+// //             </div>
+// //           </div>`; */}
