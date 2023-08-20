a4d5c403e49de0590f78466e7be51241a56ea4dd
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-08-20 18:46:05 +0300

    fix

--- src/js/apitasty.js
+++ src/js/apitasty.js
@@ -19,28 +19,10 @@ axios.interceptors.response.use(
 
 async function fetchMasterClass() {
   const response = await axios.get('');
-  // const masterClass = response.data;
+
   console.log(response.data);
 
   return response.data;
 }
 
 export { fetchMasterClass };
-
-// const INITIAL_REQUEST = `https://tasty-treats-backend.p.goit.global/api/events`;
-
-// export async function fetchMasterClass() {
-//   try {
-//     const response = await axios.get(INITIAL_REQUEST);
-
-//     if (response.status === 404) {
-//       throw new Error(response.status);
-//     }
-
-//     return response.data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// export { fetchMasterClass };
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
-import Swiper, { Pagination, Navigation } from 'swiper';
+import Swiper, { Pagination, Navigation, Autoplay } from 'swiper';
 import 'swiper/swiper-bundle.min.css';
 import Swiper from 'swiper';
 import { fetchMasterClass } from './apiTasty';
 
-// import 'swiper/swiper-bundle.min.css';
-// import 'swiper/swiper-bundle.min.mjs';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper-bundle.min.mjs';
 // import Swiper, { Pagination, Navigation } from 'swiper';
 
-const swiperContainerEl = document.querySelector('.swiper');
+// const swiperContainerEl = document.querySelector('.swiper');
 const swiperWrapperEl = document.querySelector('.swiper-wrapper');
 
 async function renderGallery() {
@@ -17,50 +17,58 @@ async function renderGallery() {
     const markup = masterClassData
       .map(item => {
         return `
-          <div class="swiper-slide">
-            <div class="event">
-              <div class="cook" style="background-image: url(${item.cook.imgUrl});"></div>
-              <div class="preview-deash-card">
-                <div class="preview-deash" style="background-image: url(${item.topic.previewUrl});"></div>
-                <p class="deash-name">${item.topic.name}</p>
-                <p class="deash-area">${item.topic.area}</p>
+          <div class="swiper-slide" id=""twist">
+            <div class="hero">
+              <div class="cook" style="background-image: url(${item.cook.imgUrl});">
+							</div>
+              <div class="preview-dish-card">
+                <div class="preview-dish" style="background-image: url(${item.topic.previewUrl});"></div>
+                <p class="dish-name">${item.topic.name}</p>
+                <p class="dish-area">${item.topic.area}</p>
               </div>
-              <div class="deash" style="background-image: url(${item.topic.previewUrl});"></div>
+              <div class="dish" style="background-image: url(${item.topic.previewUrl});"></div>
             </div>
           </div>
         `;
       })
       .join('');
 
-    swiperContainerEl.insertAdjacentHTML('beforeend', markup);
+    swiperWrapperEl.insertAdjacentHTML('beforeend', markup);
   }
+  // const swiper = new Swiper('.image-slider', {
+  //   modules: [Pagination, Navigation, Autoplay],
+  //   allowSlideNext: true,
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true,
+  //   },
+  //   autoplay: {
+  //     delay: 1500,
+  //   },
+  //   speed: 800,
+  //   loop: true,
+  // });
 
-  const swiper = new Swiper('.mySwiper', {
+  const swiper = new Swiper('image-slider', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
   });
 }
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-// });
-
 renderGallery();
