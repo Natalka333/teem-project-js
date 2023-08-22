ffb82e923ba6670d138243d62f741ecb249c89c6
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-08-22 12:00:24 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -4,36 +4,36 @@ import Swiper from 'swiper';
 
 import { fetchMasterClass } from './tastyApi';
 
-fetchMasterClass()
-  .then(data => {
-    createMarkupSwiper(data);
-  })
-  .catch(error => console.log(error));
+// fetchMasterClass()
+//   .then(data => {
+//     createMarkupSwiper(data);
+//   })
+//   .catch(error => console.log(error));
 
-function createMarkupSwiper(arrSliders) {
-  const swiperWrapperEl = document.querySelector('.swiper-wrapper');
+// function createMarkupSwiper(arrSliders) {
+//   const swiperWrapperEl = document.querySelector('.swiper-wrapper');
 
-  swiperWrapperEl.insertAdjacentHTML(
-    'beforeend',
-    arrSliders
-      .map(
-        ({ cook, topic }) =>
-          `<div class="hero_swiper-slide card">
-                <div class=" hero_slider-images">
-                    <div class="hero_image-container-1"><img class="image-1" src=${cook.imgUrl} alt=${cook.name}></div>
-                    <div class="hero_image-container-2">
-                        <img class="hero_image-2" src=${topic.previewUrl} alt=${cook.name}>
-                        <h2 class="hero_image-title">${topic.name}</h2>
-                        <p class="hero_image-descraption">${topic.area}</p>
-                    </div>
-                    <div class="hero_image-container-3"><img class="image-3" src=${topic.imgUrl} alt=${cook.name}></div>
-                </div>
-            </div>`
-      )
+//   swiperWrapperEl.insertAdjacentHTML(
+//     'beforeend',
+//     arrSliders
+//       .map(
+//         ({ cook, topic }) =>
+//           `<div class="hero_swiper-slide card">
+//                 <div class=" hero_slider-images">
+//                     <div class="hero_image-container-1"><img class="image-1" src=${cook.imgUrl} alt=${cook.name}></div>
+//                     <div class="hero_image-container-2">
+//                         <img class="hero_image-2" src=${topic.previewUrl} alt=${cook.name}>
+//                         <h2 class="hero_image-title">${topic.name}</h2>
+//                         <p class="hero_image-descraption">${topic.area}</p>
+//                     </div>
+//                     <div class="hero_image-container-3"><img class="image-3" src=${topic.imgUrl} alt=${cook.name}></div>
+//                 </div>
+//             </div>`
+//       )
 
-      .join('')
-  );
-}
+//       .join('')
+//   );
+// }
 
 const swiper = new Swiper('.image-slider', {
   modules: [Pagination, Navigation],
@@ -49,33 +49,33 @@ const swiper = new Swiper('.image-slider', {
   loop: true,
 });
 
-// fetchMasterClass()
-//   .then(data => {
-//     createMarkupSwiper(data);
-//   })
-//   .catch(error => console.log(error));
+fetchMasterClass()
+  .then(data => {
+    createMarkupSwiper(data);
+  })
+  .catch(error => console.log(error));
 
-// function createMarkupSwiper(arrSliders) {
-//   const swiperWrapperEl = document.querySelector('.swiper-wrapper');
+function createMarkupSwiper(arrSliders) {
+  const swiperWrapperEl = document.querySelector('.swiper-wrapper');
 
-//   swiperWrapperEl.insertAdjacentHTML(
-//     'beforeend',
-//     arrSliders
-//       .map(
-//         ({ cook, topic }) =>
-//           `<div class="swiper-slide" id="twist">
-//             <div class="hero">
-//               <div class="cook" style="background-image: url(${cook.imgUrl});"></div>
-//               <div class="preview-dish-card">
-//                 <div class="preview-dish" style="background-image: url(${topic.previewUrl});"></div>
-//                 <p class="dish-name">${topic.name}</p>
-//                 <p class="dish-area">${topic.area}</p>
-//               </div>
-//               <div class="dish" style="background-image: url(${topic.previewUrl});"></div>
-//             </div>
-//           </div>`
-//       )
+  swiperWrapperEl.insertAdjacentHTML(
+    'beforeend',
+    arrSliders
+      .map(
+        ({ cook, topic }) =>
+          `<div class="swiper-slide" id="twist">
+            <div class="hero">
+              <div class="cook" style="background-image: url(${cook.imgUrl});"></div>
+              <div class="preview-dish-card">
+                <div class="preview-dish" style="background-image: url(${topic.previewUrl});"></div>
+                <p class="dish-name">${topic.name}</p>
+                <p class="dish-area">${topic.area}</p>
+              </div>
+              <div class="dish" style="background-image: url(${topic.previewUrl});"></div>
+            </div>
+          </div>`
+      )
 
-//       .join('')
-//   );
-// }
+      .join('')
+  );
+}
